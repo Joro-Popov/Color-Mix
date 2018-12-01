f705e5887909b8b153bc73fe33eacce0ed28fa76
Georgi Popov <popov937@abv.bg>
2018-12-01 21:30:34 +0200

    Created Sandbox app for testing

---
+++ src/SandBox/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ColorMix.Data;
+using CommandLine;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SandBox
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+
+                SandBoxCode(serviceProvider);
+            }
+        }
+
+        private static void SandBoxCode(IServiceProvider serviceProvider)
+        {
+
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            services.AddDbContext<ColorMixContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
